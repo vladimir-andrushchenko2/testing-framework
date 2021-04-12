c612b18a50bd7ef275b5971436d48f00baf48e2e
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2021-04-12 15:16:30 +0300

    passes test

--- testing-framework/main.cpp
+++ testing-framework/main.cpp
@@ -102,50 +102,6 @@ void AssertTrueImpl(const T& t, const std::string& t_str, const std::string& fil
 
 #define ASSERT_HINT(expr, hint) AssertTrueImpl((expr), #expr, __FILE__, __FUNCTION__, __LINE__, (hint))
 
-std::vector<int> TakeEvens(const std::vector<int>& numbers) {
-    std::vector<int> evens;
-    for (int x : numbers) {
-        if (x % 2 == 0) {
-            evens.push_back(x);
-        }
-    }
-    return evens;
-}
-
-std::map<std::string, int> TakeAdults(const std::map<std::string, int>& people) {
-    std::map<std::string, int> adults;
-    for (const auto& [name, age] : people) {
-        if (age >= 18) {
-            adults[name] = age;
-        }
-    }
-    return adults;
-}
-
-bool IsPrime(int n) {
-    if (n < 2) {
-        return false;
-    }
-    int i = 2;
-    while (i * i <= n) {
-        if (n % i == 0) {
-            return false;
-        }
-        ++i;
-    }
-    return true;
-}
-
-std::set<int> TakePrimes(const std::set<int>& numbers) {
-    std::set<int> primes;
-    for (int number : numbers) {
-        if (IsPrime(number)) {
-            primes.insert(number);
-        }
-    }
-    return primes;
-}
-
 class Synonyms {
 public:
     void Add(const std::string& first_word, const std::string& second_word) {
@@ -178,28 +134,29 @@ private:
 
 void TestAddingSynonymsIncreasesTheirCount() {
     Synonyms synonyms;
-    assert(synonyms.GetSynonymCount("music"s) == 0);
-    assert(synonyms.GetSynonymCount("melody"s) == 0);
+    ASSERT_EQUAL(synonyms.GetSynonymCount("music"s), 0);
+    ASSERT_EQUAL(synonyms.GetSynonymCount("melody"s), 0);
 
     synonyms.Add("music"s, "melody"s);
-    assert(synonyms.GetSynonymCount("music"s) == 1);
-    assert(synonyms.GetSynonymCount("melody"s) == 1);
+    ASSERT_EQUAL(synonyms.GetSynonymCount("music"s), 1);
+    ASSERT_EQUAL(synonyms.GetSynonymCount("melody"s), 1);
 
     synonyms.Add("music"s, "tune"s);
-    assert(synonyms.GetSynonymCount("music"s) == 2);
-    assert(synonyms.GetSynonymCount("tune"s) == 1);
-    assert(synonyms.GetSynonymCount("melody"s) == 1);
+    ASSERT_EQUAL(synonyms.GetSynonymCount("music"s), 2);
+    ASSERT_EQUAL(synonyms.GetSynonymCount("tune"s), 1);
+    ASSERT_EQUAL(synonyms.GetSynonymCount("melody"s), 1);
 }
 
 void TestAreSynonyms() {
     Synonyms synonyms;
-    
-    synonyms.Add("color", "hue");
-    assert(synonyms.AreSynonyms("color", "hue") == true);
-    assert(synonyms.AreSynonyms("hue", "color") == true);
-    
-    // not added
-    assert(synonyms.AreSynonyms("goood", "great") == false);
+    synonyms.Add("winner"s, "champion"s);
+    synonyms.Add("good"s, "nice"s);
+
+    ASSERT(synonyms.AreSynonyms("winner"s, "champion"s));
+    ASSERT(synonyms.AreSynonyms("champion"s, "winner"s));
+
+    ASSERT(!synonyms.AreSynonyms("good"s, "champion"s));
+    ASSERT(synonyms.AreSynonyms("good"s, "nice"s));
 }
 
 void TestSynonyms() {
@@ -210,32 +167,32 @@ void TestSynonyms() {
 int main() {
     TestSynonyms();
 
-    Synonyms synonyms;
-
-    std::string line;
-    while (getline(std::cin, line)) {
-        std::istringstream command(line);
-        std::string action;
-        command >> action;
-
-        if (action == "ADD"s) {
-            std::string first_word, second_word;
-            command >> first_word >> second_word;
-            synonyms.Add(first_word, second_word);
-        } else if (action == "COUNT"s) {
-            std::string word;
-            command >> word;
-            std::cout << synonyms.GetSynonymCount(word) << std::endl;
-        } else if (action == "CHECK"s) {
-            std::string first_word, second_word;
-            command >> first_word >> second_word;
-            if (synonyms.AreSynonyms(first_word, second_word)) {
-                std::cout << "YES"s << std::endl;
-            } else {
-                std::cout << "NO"s << std::endl;
-            }
-        } else if (action == "EXIT"s) {
-            break;
-        }
-    }
+//    Synonyms synonyms;
+//
+//    std::string line;
+//    while (getline(std::cin, line)) {
+//        std::istringstream command(line);
+//        std::string action;
+//        command >> action;
+//
+//        if (action == "ADD"s) {
+//            std::string first_word, second_word;
+//            command >> first_word >> second_word;
+//            synonyms.Add(first_word, second_word);
+//        } else if (action == "COUNT"s) {
+//            std::string word;
+//            command >> word;
+//            std::cout << synonyms.GetSynonymCount(word) << std::endl;
+//        } else if (action == "CHECK"s) {
+//            std::string first_word, second_word;
+//            command >> first_word >> second_word;
+//            if (synonyms.AreSynonyms(first_word, second_word)) {
+//                std::cout << "YES"s << std::endl;
+//            } else {
+//                std::cout << "NO"s << std::endl;
+//            }
+//        } else if (action == "EXIT"s) {
+//            break;
+//        }
+//    }
 }
