759a24034477973f285407642604cf8cbc277888
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2021-04-12 12:22:26 +0300

    3rd lesson successful

--- testing-framework/main.cpp
+++ testing-framework/main.cpp
@@ -4,9 +4,14 @@
 
 using namespace std;
 
-void Assert(bool value, const string& hint) {
-    if (!value) {
-        cout << "Assertion failed.";
+template <typename T, typename U>
+void AssertEqualImpl(const T& t, const U& u, const string& t_str, const string& u_str, const string& file,
+                     const string& func, unsigned line, const string& hint) {
+    if (t != u) {
+        cout << boolalpha;
+        cout << file << "("s << line << "): "s << func << ": "s;
+        cout << "ASSERT_EQUAL("s << t_str << ", "s << u_str << ") failed: "s;
+        cout << t << " != "s << u << "."s;
         if (!hint.empty()) {
             cout << " Hint: "s << hint;
         }
@@ -15,10 +20,36 @@ void Assert(bool value, const string& hint) {
     }
 }
 
+template <typename T>
+void AssertTrueImpl(const T& t, const string& t_str, const string& file,
+                     const string& func, unsigned line, const string& hint) {
+    if (!t) {
+        cout << boolalpha;
+        
+        cout << file << "("s << line << "): "s << func << ": "s;
+        cout << "ASSERT("s << t_str << ") failed."s;
+        
+        if (!hint.empty()) {
+            cout << " Hint: "s << hint;
+        }
+        
+        cout << endl;
+        
+        abort();
+    }
+}
+
+
+#define ASSERT_EQUAL(a, b) AssertEqualImpl((a), (b), #a, #b, __FILE__, __FUNCTION__, __LINE__, ""s)
+
+#define ASSERT_EQUAL_HINT(a, b, hint) AssertEqualImpl((a), (b), #a, #b, __FILE__, __FUNCTION__, __LINE__, (hint))
+
+#define ASSERT(expr) AssertTrueImpl((expr), #expr, __FILE__, __FUNCTION__, __LINE__, ""s)
+
+#define ASSERT_HINT(expr, hint) AssertTrueImpl((expr), #expr, __FILE__, __FUNCTION__, __LINE__, (hint))
+
 int main() {
-    const string greeting = "Hello"s;
-    // Намеренная ошибка в условии, чтобы показать работу Assert
-    Assert(greeting.empty(), "Greeting must be non-empty"s);
-    // Следующая строка не должна выполниться, так как Assert аварийно завершит работу программы
-    cout << "This line will not be printed"s << endl;
+    string hello = "hello"s;
+    ASSERT(!hello.empty());
+    ASSERT_HINT(2 + 2 == 5, "This will fail"s);
 }
