fb35a83e15c687b53942f7dd0f21589de1132f79
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2021-04-12 12:41:55 +0300

    fixed standard namespace

--- testing-framework/main.cpp
+++ testing-framework/main.cpp
@@ -1,39 +1,87 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include <set>
+#include <map>
 
-using namespace std;
+using namespace std::string_literals;
+
+template<typename First, typename Second>
+std::ostream& operator<<(std::ostream& out, const std::pair<First, Second>& container) {
+    out << container.first << ": "s << container.second;
+    return out;
+}
+
+template<typename Container>
+void Print(std::ostream& out, const Container& container) {
+    bool isFirst = true;
+    for (const auto& element : container) {
+        if(isFirst) {
+            out << element;
+            isFirst = false;
+            continue;
+        }
+        out << ", "s << element;
+    }
+}
+
+// [элемент1, элемент2, элемент3, ... элементN]
+template<typename Element>
+std::ostream& operator<<(std::ostream& out, const std::vector<Element>& container) {
+    out << '[';
+    Print(out, container);
+    out << ']';
+    return out;
+}
+
+// {элемент1, элемент2, элемент3, ... элементN}
+template<typename Element>
+std::ostream& operator<<(std::ostream& out, const std::set<Element>& container) {
+    out << '{';
+    Print(out, container);
+    out << '}';
+    return out;
+}
+
+// {ключ1: значение1, ключ2: значение2, ... ключN: значениеN}
+template<typename Key, typename Value>
+std::ostream& operator<<(std::ostream& out, const std::map<Key, Value>& container) {
+    out << '{';
+    Print(out, container);
+    out << '}';
+    return out;
+}
 
 template <typename T, typename U>
-void AssertEqualImpl(const T& t, const U& u, const string& t_str, const string& u_str, const string& file,
-                     const string& func, unsigned line, const string& hint) {
+void AssertEqualImpl(const T& t, const U& u, const std::string& t_str, const std::string& u_str, const std::string& file,
+                     const std::string& func, unsigned line, const std::string& hint) {
     if (t != u) {
-        cout << boolalpha;
-        cout << file << "("s << line << "): "s << func << ": "s;
-        cout << "ASSERT_EQUAL("s << t_str << ", "s << u_str << ") failed: "s;
-        cout << t << " != "s << u << "."s;
+        std::cout << std::boolalpha;
+        std::cout << file << "("s << line << "): "s << func << ": "s;
+        std::cout << "ASSERT_EQUAL("s << t_str << ", "s << u_str << ") failed: "s;
+        std::cout << t << " != "s << u << "."s;
         if (!hint.empty()) {
-            cout << " Hint: "s << hint;
+            std::cout << " Hint: "s << hint;
         }
-        cout << endl;
+        std::cout << std::endl;
         abort();
     }
 }
 
 template <typename T>
-void AssertTrueImpl(const T& t, const string& t_str, const string& file,
-                     const string& func, unsigned line, const string& hint) {
+void AssertTrueImpl(const T& t, const std::string& t_str, const std::string& file,
+                     const std::string& func, unsigned line, const std::string& hint) {
     if (!t) {
-        cout << boolalpha;
+        std::cout << std::boolalpha;
         
-        cout << file << "("s << line << "): "s << func << ": "s;
-        cout << "ASSERT("s << t_str << ") failed."s;
+        std::cout << file << "("s << line << "): "s << func << ": "s;
+        std::cout << "ASSERT("s << t_str << ") failed."s;
         
         if (!hint.empty()) {
-            cout << " Hint: "s << hint;
+            std::cout << " Hint: "s << hint;
         }
         
-        cout << endl;
+        std::cout << std::endl;
         
         abort();
     }
@@ -49,7 +97,7 @@ void AssertTrueImpl(const T& t, const string& t_str, const string& file,
 #define ASSERT_HINT(expr, hint) AssertTrueImpl((expr), #expr, __FILE__, __FUNCTION__, __LINE__, (hint))
 
 int main() {
-    string hello = "hello"s;
+    std::string hello = "hello"s;
     ASSERT(!hello.empty());
     ASSERT_HINT(2 + 2 == 5, "This will fail"s);
 }
